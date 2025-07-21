fe47d795c3e1b9f8951dc0127c23a791eb021e66
kevinfacundopereyra <pereyrakevinfacundo@gmail.com>
2025-07-21 02:46:30 -0300

    styles

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const addUserBtn = document.getElementById("addUserBtn");
 addUserBtn.addEventListener("click", addUser);
 
 render();
+
+const calcDebtBtn = document.getElementById("calcDebtBtn");
+calcDebtBtn.addEventListener("click", () => {
+  console.log("click desde el calcDebtBtn");
+});
--- src/services.js
+++ src/services.js
@@ -1,21 +1,28 @@
-import { checkName, addNewUser, getUsers } from "./state/usersState.js";
+import User from "./models/User.js";
 import { renderUsersList } from "./ui/usersUi.js";
 
 const nameInput = document.getElementById("nameInput");
 const usersUl = document.getElementById("usersUl");
 
+const newUsersList = [];
+
 function render() {
-  renderUsersList(usersUl, getUsers());
+  renderUsersList(usersUl, newUsersList);
 }
 
 function addUser() {
   const name = nameInput.value.trim();
-  if (checkName(name)) {
-    addNewUser(name);
+
+  const nameExists = newUsersList.some((user) => user.name === name);
+
+  if (name && !nameExists) {
+    const newUser = new User(name);
+    newUsersList.unshift(newUser);
     render();
     nameInput.value = "";
   } else {
     alert("El nombre no puede estar repetido o vacío");
+    nameInput.value = "";
   }
 }
 
